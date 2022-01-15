e491df2b52da6d53395b7f18293709e8d1772386
Luís Fernando de Sá Amorim <40233089+MoonCancerBR@users.noreply.github.com>
2022-01-14 22:54:14 -0300

    Delete Atividade7/Front/src/keys directory

--- Atividade7/Front/src/keys/firebase_key.js
+++
@@ -1,9 +0,0 @@
-var key = {
-    apiKey: "AIzaSyCCfwwwV9Fdsqvdb1UOj5Qf7JF1d1djNnY",
-    authDomain: "atividade07-5a496.firebaseapp.com",
-    projectId: "atividade07-5a496",
-    storageBucket: "atividade07-5a496.appspot.com",
-    messagingSenderId: "373127049401",
-    appId: "1:373127049401:web:5710fd3da2f03d43ff6b68"
-  }
-export default key
