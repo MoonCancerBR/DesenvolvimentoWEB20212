951da0c6bbe3a9cb40beee4ffef2c5c0f0ed0ef6
Luís Fernando de Sá Amorim <40233089+MoonCancerBR@users.noreply.github.com>
2022-01-14 23:15:39 -0300

    Update Edit.jsx

--- Atividade7/Front/src/components/Edit.jsx
+++ Atividade7/Front/src/components/Edit.jsx
@@ -1,27 +1,21 @@
 import React, { Component } from 'react'
-
 import FirebaseContext from '../utils/FirebaseContext'
 import FirebaseService from '../services/FirebaseService'
 
 const EditPage = (props) => (
     <FirebaseContext.Consumer>
-        {contexto => <Edit firebase={contexto} id={props.match.params._id} />}
+        {contexto => <Edit firebase={contexto} id={props.match.params.id} />}
     </FirebaseContext.Consumer>
 )
-
 class Edit extends Component {
-
     constructor(props) {
         super(props)
-
         this.state = { nome: '', curso: '', IRA: '' }
-
         this.setNome = this.setNome.bind(this)
         this.setCurso = this.setCurso.bind(this)
         this.setIRA = this.setIRA.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
     }
-
     componentDidMount() {
         FirebaseService.retrieve(
             this.props.firebase.getFirestore(),
@@ -32,52 +26,41 @@ class Edit extends Component {
                         curso: estudante.curso,
                         IRA: estudante.IRA
                     })
-
             },
-            this.props._id
+            this.props.id
         )
     }
-
     setNome(e) {
         this.setState({ nome: e.target.value })
     }
-
     setCurso(e) {
         this.setState({ curso: e.target.value })
     }
-
     setIRA(e) {
         this.setState({ IRA: e.target.value })
     }
-
     onSubmit(e) {
         e.preventDefault()
-
         const estudante = {
             nome: this.state.nome,
             curso: this.state.curso,
             IRA: this.state.IRA
         }
-
         FirebaseService.edit(
             this.props.firebase.getFirestore(),
             (mensagem) => {
                 if(mensagem==='ok') console.log(`Estudante atualizado com sucesso.`)
             },
             estudante,
-            this.props._id
+            this.props.id
         )
-
         this.setState({ nome: '', curso: '', IRA: '' })
-
     }
-
     render() {
         return (
             <div style={{ marginTop: 10 }}>
                 <h3>Editar Estudante</h3>
                 <form onSubmit={this.onSubmit}>
-
                     <div className="form-group">
                         <label>Nome: </label>
                         <input type="text" className="form-control"
@@ -93,15 +76,12 @@ class Edit extends Component {
                         <input type="text" className="form-control"
                             value={this.state.IRA} onChange={this.setIRA} />
                     </div>
-
                     <div className="form-group">
                         <input type="submit" value="Editar Estudante" className="btn btn-primary" />
                     </div>
                 </form>
-
             </div>
         )
     }
 }
-
 export default EditPage
